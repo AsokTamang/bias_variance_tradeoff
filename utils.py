1925199550c0f9474e725803551550ac69971c36
AsokTamang <ashoktmg2058@gmail.com>
2026-03-10 23:03:45 -0400

    reading the file

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler,PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 
@@ -10,10 +12,42 @@ def split_data(X,Y):
     X_train, X_, Y_train, Y_ = train_test_split(X, Y, test_size = 0.4, random_state = 1)
     X_cv,X_test,Y_cv,Y_test = train_test_split(X_, Y_, test_size = 0.5, random_state = 1)
     del X_,Y_
-    return X_train,X_cv,X_test,Y_train,Y_cv,Y_test
+    return X_train,Y_train,X_cv,Y_cv,X_test,Y_test
 
 
 
 def plot_error_rate_polydegree(degrees,X_train,X_cv,Y_train,Y_cv):
+    training_error = []
+    cv_error = []
+    degrees_history = []
     for i in range(degrees):
+        poly = PolynomialFeatures(degree=i,include_bias=False)
+        sc = StandardScaler()
+        lr = LinearRegression()
+        X_train_poly = poly.fit_transform(X_train)  #polynomial feature engineering for training data
+        X_train_scaled = sc.fit_transform(X_train_poly)  #standard scaling for training data
+
+        X_cv_poly = poly.transform(X_cv)    #polynomial feature engineering for cross_validation dataset
+        X_cv_scaled = sc.transform(X_cv_poly)  #standard scaling for polynomial dataset
+
+        lr.fit(X_train_scaled,Y_train)  #training the model
+        predicted_Y_train = lr.predict(X_train_scaled)
+        training_error.append(mean_squared_error(predicted_Y_train,Y_train) / 2)
+
+        predicted_Y_cv = lr.predict(X_cv_scaled)
+        cv_error.append(mean_squared_error(predicted_Y_cv,Y_cv) / 2)
+        degrees_history.append(i)
+
+    plt.plot(degrees_history,training_error,label="Training Error",c='b')
+    plt.plot(degrees_history,cv_error,label="CV Error",c='r')
+    plt.xlabel("Degrees")
+    plt.ylabel("Mean Squared Error")
+    plt.show()
+
+
+
+
+
+
+
 
